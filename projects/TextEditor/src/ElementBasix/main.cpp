c414b2c98ca84563de90d3031b09470396988d43
Sam Ramirez <ramirezsam99@gmail.com>
2019-08-30 14:27:50 -0700

    Added Inital message and a border

--- projects/TextEditor/src/ElementBasix/main.cpp
+++ projects/TextEditor/src/ElementBasix/main.cpp
@@ -4,6 +4,7 @@
  */
 
 #include <iostream>
+#include <string>
 #include <curses.h>
 #include <panel.h>
 
@@ -11,38 +12,47 @@ using namespace std;
 
 int main(){
 	WINDOW* main_window = nullptr;
-	int num_cols = 0;
-	int num_rows = 0;
+	int num_cols = 177;
+	int num_rows = 52;
 	//Initializes Window
 	main_window = initscr();
+
 	//resize WINDOW
 	getmaxx(main_window);
 	resize_term(num_rows - 1, num_cols - 1);
+
 	//turn off keyboard echo
 	noecho();
 	//Turn on keyboard input
 	keypad(main_window, TRUE);
-
 	//Hide cursor
 	curs_set(FALSE);
+
 	// Main program logic
 	for(int i = 0; i < num_cols; i++) {
-		mvaddch(0, i, ACS_BLOCK);
-
-		mvaddch(num_rows - 1, i, ACS_BLOCK);
+		mvaddch(0, i, '#');
+		mvaddch(num_rows - 1, i, '*');
 	}
+
 	for(int i = 0; i < num_rows; i++) {
-		mvaddch(i, 0, ACS_BLOCK);
-
-		mvaddch(i, num_cols - 1, ACS_BLOCK);
+		char num[3];
+		int index = i + 1;
+		string sidebar = to_string(index);
+		strcpy(num, sidebar.c_str());
+		mvaddstr(i + 1, 0, num);
 	}
 
+	//add initial greeting when opening application
+	string greeting = "Welcome to Basix";
+	char greet[greeting.length() + 1];
+	strcpy(greet, greeting.c_str());
+	mvaddstr(20, 73, greet);
 	//refresh tells curses to draw everything
 	refresh();
-	mvaddch(10, 10, 'X');
+
 	//pause for user input;
 	char input = getch();
-
-	//endwin();
+	//Ends program when user inputs n
+	endwin();
 	return 0;
 }
