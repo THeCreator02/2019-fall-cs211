fda30ef09584369f87dc4ca61cc5d2af98cb6054
Sam Ramirez <ramirezsam99@gmail.com>
2019-09-05 10:11:15 -0700

    Message now appears when first typing, added current time as well

--- projects/TextEditor/src/ElementBasix/main.cpp
+++ projects/TextEditor/src/ElementBasix/main.cpp
@@ -7,19 +7,43 @@
 #include <string>
 #include <curses.h>
 #include <panel.h>
+#include <time.h>
 
 using namespace std;
 
+char* convert(string message, char* names){
+	char name[message.size() + 1];
+	strcpy(name, message.c_str());
+	names = name;
+	cout << names << endl;
+	return names;
+}
+
 int main(){
+	// string name = "Hello my name is Sam";
+	// char* sam;
+	// sam = convert(name, sam);
+	// cout << sam << endl;
+
+	//Grabs current time in string format
+	time_t rawtime;
+	struct tm * timeinfo;
+	time ( &rawtime );
+	timeinfo = localtime ( &rawtime );
+	string curTime = asctime(timeinfo);
+
+	//Sets up window in terminal
 	WINDOW* main_window = nullptr;
-	int num_cols = 177;
-	int num_rows = 52;
+
+	int width;
+	int height;
+
 	//Initializes Window
 	main_window = initscr();
 
 	//resize WINDOW
-	getmaxx(main_window);
-	resize_term(num_rows - 1, num_cols - 1);
+	getmaxyx(main_window, height, width);
+	resize_term(height - 1, width - 1);
 
 	//turn off keyboard echo
 	noecho();
@@ -27,14 +51,14 @@ int main(){
 	keypad(main_window, TRUE);
 	//Hide cursor
 	curs_set(FALSE);
+	cbreak();
 
-	// Main program logic
-	for(int i = 0; i < num_cols; i++) {
+	// Adds a border at the top of the screen;
+	for(int i = 0; i < width; i++) {
 		mvaddch(0, i, '#');
-		mvaddch(num_rows - 1, i, '*');
 	}
-
-	for(int i = 0; i < num_rows; i++) {
+	//adds line numbers
+	for(int i = 0; i < height; i++) {
 		char num[3];
 		int index = i + 1;
 		string sidebar = to_string(index);
@@ -42,17 +66,52 @@ int main(){
 		mvaddstr(i + 1, 0, num);
 	}
 
-	//add initial greeting when opening application
-	string greeting = "Welcome to Basix";
-	char greet[greeting.length() + 1];
-	strcpy(greet, greeting.c_str());
-	mvaddstr(20, 73, greet);
-	//refresh tells curses to draw everything
+	//Adds current time to bottom of window
+	char currentTime[curTime.size() + 1];
+	strcpy(currentTime, curTime.c_str());
+	mvaddstr(height - 2, width - 30, currentTime);
 	refresh();
 
-	//pause for user input;
-	char input = getch();
-	//Ends program when user inputs n
+	//Displays the initial message on the screen
+	char line;
+	while(line == '\0') {
+		refresh();
+		//grabs a character from the user
+		line = getch();
+		//Adds greeting to the middle of the screen
+		string greeting = "Welcome to Basix";
+		char greet[greeting.size() + 1];
+		strcpy(greet, greeting.c_str());
+		//Aligns the greeting to the center of the screen
+		int y = height / 2;
+		y -= 1;
+		int x = width / 2;
+		x -= 10;
+		mvaddstr(y, x, greet);
+		//Resets time after adding message
+		time ( &rawtime );
+		timeinfo = localtime ( &rawtime );
+		string curTime = asctime(timeinfo);
+		char currentTime[curTime.size() + 1];
+		strcpy(currentTime, curTime.c_str());
+		mvaddstr(height - 2, width - 30, currentTime);
+		refresh();
+	}
+
+	//Is suppose to display the character to the screen
+	char typing;
+	wmove(main_window, 1, 2);
+	curs_set(TRUE);
+	wrefresh(main_window);
+	typing = getch();
+	waddch(main_window, typing);
+	time (&rawtime);
+	timeinfo = localtime (&rawtime);
+	curTime = asctime(timeinfo);
+	strcpy(currentTime, curTime.c_str());
+	mvaddstr(height - 2, width - 30, currentTime);
+	refresh();
+
 	endwin();
 	return 0;
 }
